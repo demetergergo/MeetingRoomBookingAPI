a2f897723b02019862e84450aaa6ebcafa35f6e2
demetergergo6 <demetergergo6@gmail.com>
2025-07-18 12:05:19 +0200

    Initial commit

---
+++ src/test/java/com/meetingroom/MeetingRoomBookingAPI/MeetingRoomBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingroom.MeetingRoomBookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
