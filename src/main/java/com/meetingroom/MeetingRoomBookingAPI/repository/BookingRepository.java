ce3fe5dddd3097d42baeedf0b93345115a2232ac
demetergergo6 <demetergergo6@gmail.com>
2025-07-18 13:09:52 +0200

    Add methods to BookingRepository for finding bookings by meeting room name and checking for overlapping bookings

--- src/main/java/com/meetingroom/MeetingRoomBookingAPI/repository/BookingRepository.java
+++ src/main/java/com/meetingroom/MeetingRoomBookingAPI/repository/BookingRepository.java
@@ -2,9 +2,15 @@ package com.meetingroom.MeetingRoomBookingAPI.repository;
 
 import com.meetingroom.MeetingRoomBookingAPI.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    List<Booking> findByMeetingRoomName(String roomName);
 
+    @Query("SELECT b FROM Booking b WHERE b.meetingRoom.id = :roomId AND b.startTime < :endTime AND b.endTime > :startTime")
+    List<Booking> findOverlappingBookings(UUID roomId, LocalDateTime startTime, LocalDateTime endTime);
 }
